979a18c75b19714eb715fa6e5521570f17df2ba6
Depender Kumar Soni <46902839+DependerKumarSoni@users.noreply.github.com>
2022-03-18 02:00:45 +0530

    Final Comit

---
+++ score_file.py
@@ -0,0 +1,10 @@
+import json
+
+def dump_score(self):
+    final_high_score = 
+    filename = 'final_high_score.json'
+    with open (filename, 'w') as f_obj:
+        json.dump(final_high_score, f_obj)
+
+def load_score(self):
+
